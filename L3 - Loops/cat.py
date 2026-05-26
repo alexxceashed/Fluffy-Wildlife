fea0c458351bbea7a8bcff29871114654a968a1f
Alex <alexxceashed@proton.me>
2026-05-26 16:55:55 +0530

    loops.

---
+++ L3 - Loops/cat.py
@@ -0,0 +1,17 @@
+
+    
+#while loops- used for executing a block of code repeatedly as long as a given condition is true. 
+#The condition is evaluated before executing the block of code. 
+#If the condition is false, the loop will not execute at all.
+
+'''In this example, the loop will execute as long as the value of i is not equal to 0.'''
+# i = 3
+# while i != 0:
+#     print("Meow")
+#     i -= 1
+
+
+#List - a datatype that can hold multiple values.
+#Lists are ordered, changeable, and allow duplicate values.
+for i in [0, 1, 2]:
+    print("Meow")
